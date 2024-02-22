3281dd81e643f97b63d2209b453a80661fbe96fe
Ihor <mrshevchenko33@gmail.com>
2024-02-22 10:31:06 +0200

    Change if to elif

--- main.py
+++ main.py
@@ -3,13 +3,13 @@ second_num = int(input('Please enter second number:  '))
 action = input('Please enter action:  ')
 
 if action == '*':
-    print(first_num * second_num)
-if action == '+':
-    print(first_num + second_num)
-if action == '-':
-    print(first_num - second_num)
-if action == '/':
+    print('Your result: ', first_num * second_num)
+elif action == '+':
+    print('Your result: ', first_num + second_num)
+elif action == '-':
+    print('Your result: ', first_num - second_num)
+elif action == '/':
     if second_num == 0:
         print("You can't divide by 0")
     else:
-        print(first_num/second_num)
+        print('Your result: ', first_num/second_num)
